3b99223cb06b1300b84015b98d7864476ee21121
halfg33k <johnsonam17@gmail.com>
2018-07-24 16:30:07 -0400

    setup opportunities blog

--- single.php
+++ single.php
@@ -1,14 +1,20 @@
 <?php get_header(); ?>
-  <main id="main">
-    <section class="content">
-      <div class="container">
-        <?php if(have_posts()): while(have_posts()): the_post(); ?>
-          <h1 class="page-title"><?php the_title(); ?></h1>
-          <article>
-            <?php the_content(); ?>
-          </article>
-        <?php endwhile; endif; ?>
-      </div>
-    </section>
-  </main>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 content-with-sidebar">
+                <?php if(have_posts()): while(have_posts()): the_post(); ?>
+                    <article>
+                        <h2><?php the_title(); ?></h2>
+                        <hr class="hr--light" />
+                        <?php the_content(); ?>
+                    </article>
+                <?php endwhile; endif; ?>
+            </div>
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</section>
+
 <?php get_footer(); ?>

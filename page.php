fa613cf196895519e03468ae494a3bcc444831c0
John <jcampbell@childressagency.com>
2018-07-30 15:28:17 -0400

    defferent layout for events pages

--- page.php
+++ page.php
@@ -2,6 +2,18 @@
 
 <section>
     <div class="container">
+      <?php 
+        if(is_page('events') || is_archive('events') || is_singular('event')){
+          if(have_posts()){
+            while(have_posts()){
+              the_post();
+              echo '<article>';
+              the_content();
+              echo '</article>';
+            }
+          }
+        }
+      else{ ?>
         <div class="row">
             <div class="col-md-9 content-with-sidebar">
                 <?php //get_template_part( 'tp-share' ); ?>
@@ -23,6 +35,7 @@
             </div>
             <?php get_sidebar(); ?>
         </div>
+      <?php } ?>
     </div>
 </section>
 

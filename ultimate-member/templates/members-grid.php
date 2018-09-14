ce211f7373e684f1f8d89e0d194a65dc6d78090a
childressdev <jcampbell@childressagency.com>
2018-09-14 16:02:42 -0400

    modfied read more bio link

--- ultimate-member/templates/members-grid.php
+++ ultimate-member/templates/members-grid.php
@@ -19,6 +19,9 @@
       
       <?php 
         $i = 0; 
+
+        //add_filter('um_prepare_user_query_args', 'accr_prepare_user_query_args', 10, 2);
+        //var_dump($accr_paid_members);
         foreach(um_members('users_per_page') as $member):
           $i++; 
           um_fetch_user($member);
@@ -68,12 +71,13 @@
                   foreach($tagline_fields as $key){
                     if($key /*&& um_filtered_value( $key )*/){
                       $value = um_filtered_value($key);
-                      $value = strlen($value) > 245 ? substr($value, 0, 245) . '<a href="' . um_user_profile_url() . '">...[read more]</a>' : $value;
+                      $value = strlen($value) > 245 ? substr($value, 0, 245) . '<a href="' . um_user_profile_url() . '"> [...] </a>' : $value;
                     if(!$value){ continue; } ?>
             
                     <div class="um-member-tagline um-member-tagline-<?php echo $key;?>">
                       <div class="um-member-name"><a href="<?php echo um_user_profile_url(); ?>" title="<?php echo $display_name; ?>"><?php echo $display_name; ?></a></div>
                       <?php _e( $value, 'ultimate-member'); ?>
+                      <p class="read-more-about" style="font-weight:500;"><a href="<?php echo um_user_profile_url(); ?>">Read more about <?php echo $display_name; ?>...</a></p>
                     </div>
               <?php }} //end foreach, end if
                 }//end hover bio ?>
@@ -95,7 +99,7 @@
       <div class="um-clear"></div>
 
     </div>
-    
+    <?php //do_action( 'um_members_directory_footer', $args ); ?>
   </div>
 
   <div class="tab-pane fade" id="all" role="tab-panel" aria-labelledby="nav-all">
@@ -105,6 +109,7 @@
       
       <?php 
         $i = 0; 
+        //remove_filter('um_prepare_user_query_args', 'accr_prepare_user_query_args', 10, 2);
         foreach(um_members('users_per_page') as $member):
           $i++; 
           um_fetch_user($member); ?>
@@ -153,12 +158,14 @@
                   foreach($tagline_fields as $key){
                     if($key /*&& um_filtered_value( $key )*/){
                       $value = um_filtered_value($key);
-                      $value = strlen($value) > 245 ? substr($value, 0, 245) . '<a href="' . um_user_profile_url() . '">...[read more]</a>' : $value;
+                      $value = strlen($value) > 245 ? substr($value, 0, 245) . '<a href="' . um_user_profile_url() . '"> [...]</a>' : $value;
                     if(!$value){ continue; } ?>
             
                     <div class="um-member-tagline um-member-tagline-<?php echo $key;?>">
                       <div class="um-member-name"><a href="<?php echo um_user_profile_url(); ?>" title="<?php echo $display_name; ?>"><?php echo $display_name; ?></a></div>
                       <?php _e( $value, 'ultimate-member'); ?>
+                      <p class="read-more-about" style="font-weight:500;"><a href="<?php echo um_user_profile_url(); ?>">Read more about <?php echo $display_name; ?>...</a></p>
+
                     </div>
               <?php }} //end foreach, end if
                 }//end hover bio ?>
@@ -180,5 +187,6 @@
       <div class="um-clear"></div>
 
     </div>
+    <?php //do_action( 'um_members_directory_footer', $args ); ?>
   </div>
 </div>
